b9561ab3a78a5a626ddb6b5750906cb55a435b36
Martin Lowe <martinlowe673@gmail.com>
2024-11-11 17:20:48 +0000

    link

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,14 @@
 "use client"
 import { useUser } from "@clerk/nextjs"
+import Link from "next/link"
 const Header = () => {
     const {user} = useUser()
     console.log(user)
   return (
     <header className="flex flex-wrap justify-between items-center px-4 py-2">
-
+        <div>
+            <Link href="/">Shopr</Link>
+        </div>
     </header>
   )
 }
