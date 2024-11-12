094e67b62539bc84c67225e3eeb3db63c2a08132
Martin Lowe <martinlowe673@gmail.com>
2024-11-12 07:40:09 +0000

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <header className="flex flex-wrap justify-between items-center px-4 py-2">
         <div>
-            <Link href="/">Shopr</Link>
+            <Link href="/" className="text-2xl font-bold text-blue-500 hover:opacity-50 cursor-pointer mx-auto sm:mx-0">Shopr</Link>
         </div>
     </header>
   )
